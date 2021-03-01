9742e7f38de51438db5e4953f67b61db1df431cc
TheBigWolfy <51050518+thebigwolfy@users.noreply.github.com>
2021-03-01 14:58:41 +0100

    0.1.24

--- db/mysql.js
+++ db/mysql.js
@@ -6,13 +6,13 @@ module.exports = {
 
 function mysql(hostSQL, user, passwordSQLUser, database, charset) {
 	
-	if(!hostSQL) throw new TypeError("NazmaJS - Mysql - Indicate the data connexion host!");
+	if(!hostSQL) throw new Error("NazmaJS - Mysql - Indicate the data connexion host!");
 	
-	if(!user) throw new TypeError("NazmaJS - Mysql - Indicate the data connexion user name!");
+	if(!user) throw new Error("NazmaJS - Mysql - Indicate the data connexion user name!");
 	
-	if(!passwordSQLUser) throw new TypeError("NazmaJS - Mysql - Indicate the data connexion password user!");
+	if(!passwordSQLUser) throw new Error("NazmaJS - Mysql - Indicate the data connexion password user!");
 	
-	if(!database) throw new TypeError("NazmaJS - Mysql - Indicate the data connexion name data base!");
+	if(!database) throw new Error("NazmaJS - Mysql - Indicate the data connexion name data base!");
 	
 	const MySQLjs = require("mysql"), charsetData = (charset !== undefined ? charset : "utf8mb4_bin");
 	
@@ -34,11 +34,11 @@ function mysql(hostSQL, user, passwordSQLUser, database, charset) {
 
 		if(err) {
 			
-			setTimeout(mysql(hostSQL, user, passwordSQLUser, database, charset), 10000);
+			setTimeout(mysql(hostSQL, user, passwordSQLUser, database, charsetData), 10000);
 			
-			if(err.code === "ER_HOST_IS_BLOCKED") throw new TypeError("NazmaJS - Mysql - The connexion is bloqued to the MySQL server!");
+			if(err.code === "ER_HOST_IS_BLOCKED") throw new Error("NazmaJS - Mysql - The connexion is bloqued to the MySQL server!");
 			
-			throw new TypeError("NazmaJS - Mysql - Impossible to connect to the MySQL server. Code: " + err.code);
+			throw new Error("NazmaJS - Mysql - Impossible to connect to the MySQL server. Code: " + err.code);
 			
 		} else return console.log("NazmaJS - Mysql - Connected to the MySQL server! Connexion ID: " + sql.threadId);
 		
@@ -46,13 +46,15 @@ function mysql(hostSQL, user, passwordSQLUser, database, charset) {
 	
 	sql.on("error", function (err) {
 		
-        if(err.code === "PROTOCOL_CONNECTION_LOST" && err.code !== "ER_HOST_IS_BLOCKED") {
+		if(err.code === "ERR_INVALID_CALLBACK") throw new Error("NazmaJS - Mysql - Error to connect the MySQL server. The connexion reload not possible!");
 			
-            setTimeout(mysql(hostSQL, user, passwordSQLUser, database, charset), 2000);
+		if(err.code === "PROTOCOL_CONNECTION_LOST" && err.code !== "ER_HOST_IS_BLOCKED") {
 			
-			throw new TypeError("NazmaJS - Mysql - Reload to the MySQL server...");
+            setTimeout(mysql(hostSQL, user, passwordSQLUser, database, charsetData), 2000);
 			
-        } else throw new TypeError("NazmaJS - Mysql - Error to the MySQL server. Error code:\n" + err);
+			throw new Error("NazmaJS - Mysql - Reload to the MySQL server...");
+			
+        } else throw new Error("NazmaJS - Mysql - Error to connect the MySQL server. Error code:\n" + err);
 		
 	});
 	
